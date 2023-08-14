979261aec5a3878dc8fc6527f036629b2c7b1d21
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-14 20:03:30 +0300

    Add toggleSections module

--- index.js
+++ index.js
@@ -1,104 +1,79 @@
 import getTimeNow from './modules/timeNow.js';
+import toggleSectionVisibility from './modules/toggleSection.js';
 
-class BookLibrary {
-  constructor() {
-    this.myBooks = [];
-    this.addBtn = document.getElementById('add-btn');
-    this.listBooks = document.getElementById('list-books');
-
-    const storedBooks = localStorage.getItem('myBooks');
-    if (storedBooks) {
-      this.myBooks.push(...JSON.parse(storedBooks));
-    }
-
-    this.addBtn.addEventListener('click', this.addBook.bind(this));
-
-    // Initial display of books
-    this.showBooks();
-  }
-
-  showBooks() {
-    this.listBooks.innerHTML = '';
-
-    this.myBooks.forEach((myBook) => {
-      const myBookDiv = document.createElement('div');
-      myBookDiv.classList.add('row');
-
-      const bookDetails = document.createElement('h4');
-      bookDetails.textContent = `${myBook.bookTitle} by ${myBook.bookAuthor}`;
-
-      const removeBtn = document.createElement('button');
-      removeBtn.classList.add('remove-btn');
-      removeBtn.textContent = 'Remove';
-      removeBtn.addEventListener('click', () => {
-        this.removeBook(myBook.bookID);
-      });
-      myBookDiv.appendChild(bookDetails);
-      myBookDiv.appendChild(removeBtn);
-      this.listBooks.appendChild(myBookDiv);
-    });
-  }
-
-  addBook() {
-    const bookID = this.myBooks.length + 1;
-    const bookTitle = document.getElementById('title').value.trim();
-    const bookAuthor = document.getElementById('author').value.trim();
-    const errorSpan = document.getElementById('error-msg');
-    const errorMsg = 'Both Title and Author must be filled out!';
-
-    if (bookTitle === '' || bookAuthor === '') {
-      errorSpan.textContent = errorMsg;
-      return;
-    }
-
-    this.myBooks.push({ bookID, bookTitle, bookAuthor });
-    localStorage.setItem('myBooks', JSON.stringify(this.myBooks));
-
-    // Call showBooks() to see all books
-    this.showBooks();
-
-    // Clear the input fields values
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-  }
-
-  removeBook(bookIDToRemove) {
-    const myBooksUpdated = this.myBooks.filter((myBook) => myBook.bookID !== bookIDToRemove);
-    localStorage.setItem('myBooks', JSON.stringify(myBooksUpdated));
-
-    // Update the myBooks array with the updated version
-    this.myBooks.length = 0;
-    this.myBooks.push(...myBooksUpdated);
-
-    // Call showBooks() to see all books
-    this.showBooks();
-  }
-}
-
-const bookLibrary = new BookLibrary();
-bookLibrary.showBooks();
-
-const currentTime = document.getElementById('current-time');
-currentTime.textContent = getTimeNow();
-
-const listBookSection = document.getElementById('list-books-section');
-const addBooksSection = document.getElementById('add-books-section');
-const contactSection = document.getElementById('contact-section');
+const myBooks = [];
+const addBtn = document.getElementById('add-btn');
+const listBooks = document.getElementById('list-books');
 const addBooksLink = document.getElementById('add-books-link');
 const listBooksLink = document.getElementById('list-books-link');
 const contactLink = document.getElementById('contact-link');
+const currentTime = document.getElementById('current-time');
 
-function toggleSectionVisibility(sectionId) {
-  const sections = [listBookSection, addBooksSection, contactSection];
-
-  sections.forEach((section) => {
-    if (section.id === sectionId) {
-      section.style.display = 'block';
-    } else {
-      section.style.display = 'none';
-    }
-  });
+const storedBooks = localStorage.getItem('myBooks');
+if (storedBooks) {
+  myBooks.push(...JSON.parse(storedBooks));
 }
+
+const addBook = () => {
+  const bookID = myBooks.length + 1;
+  const bookTitle = document.getElementById('title').value.trim();
+  const bookAuthor = document.getElementById('author').value.trim();
+  const errorSpan = document.getElementById('error-msg');
+  const errorMsg = 'Both Title and Author must be filled out!';
+
+  if (!bookTitle || !bookAuthor) {
+    errorSpan.textContent = errorMsg;
+    return;
+  }
+
+  myBooks.push({ bookID, bookTitle, bookAuthor });
+  localStorage.setItem('myBooks', JSON.stringify(myBooks));
+
+  // Call showBooks() to see all books
+  showBooks();
+
+  // Clear the input fields values
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+};
+
+const removeBook = (bookIDToRemove) => {
+  const myBooksUpdated = myBooks.filter((myBook) => myBook.bookID !== bookIDToRemove);
+  localStorage.setItem('myBooks', JSON.stringify(myBooksUpdated));
+
+  // Update the myBooks array with the updated version
+  myBooks.length = 0;
+  myBooks.push(...myBooksUpdated);
+
+  // Call showBooks() to see all books
+  showBooks();
+};
+
+const showBooks = () => {
+  listBooks.innerHTML = '';
+
+  myBooks.forEach((myBook) => {
+    const myBookDiv = document.createElement('div');
+    myBookDiv.classList.add('row');
+
+    const bookDetails = document.createElement('h4');
+    bookDetails.textContent = `${myBook.bookTitle} by ${myBook.bookAuthor}`;
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', () => {
+      removeBook(myBook.bookID);
+    });
+    myBookDiv.appendChild(bookDetails);
+    myBookDiv.appendChild(removeBtn);
+    listBooks.appendChild(myBookDiv);
+  });
+};
+
+addBtn.addEventListener('click', addBook);
+
+currentTime.textContent = getTimeNow();
 
 listBooksLink.addEventListener('click', () => {
   toggleSectionVisibility('list-books-section');
@@ -111,3 +86,6 @@ addBooksLink.addEventListener('click', () => {
 contactLink.addEventListener('click', () => {
   toggleSectionVisibility('contact-section');
 });
+
+// Initial display of books
+showBooks();

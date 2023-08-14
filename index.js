c1305f1c3b1697b2740818dba7354df6cbd48ec7
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-15 01:00:22 +0300

    Fix links bug, eventsListener

--- index.js
+++ index.js
@@ -5,21 +5,36 @@ import showBooks from './modules/showBooks.js';
 import addBook from './modules/addBook.js';
 
 const myBooks = [];
-const navigation = document.querySelector('nav');
+// const navigation = document.querySelector('nav');
 const addBtn = document.getElementById('add-btn');
 const currentTime = document.getElementById('current-time');
+const addBooksLink = document.getElementById('add-books-link');
+const listBooksLink = document.getElementById('list-books-link');
+const contactLink = document.getElementById('contact-link');
 
-navigation.addEventListener('click', (event) => {
-  const { target } = event;
-  if (target.tagName === 'A') {
-    event.preventDefault();
-    const sectionId = target.getAttribute('href');
-    toggleSectionVisibility(sectionId);
-  }
-});
+// navigation.addEventListener('click', (event) => {
+//   const { target } = event;
+//   if (target.tagName === 'A') {
+//     event.preventDefault();
+//     const sectionId = target.getAttribute('href');
+//     toggleSectionVisibility(sectionId);
+//   }
+// });
 
 addBtn.addEventListener('click', () => {
   addBook(myBooks);
+});
+
+listBooksLink.addEventListener('click', () => {
+  toggleSectionVisibility('list-books-section');
+});
+
+addBooksLink.addEventListener('click', () => {
+  toggleSectionVisibility('add-books-section');
+});
+
+contactLink.addEventListener('click', () => {
+  toggleSectionVisibility('contact-section');
 });
 
 currentTime.textContent = getTimeNow();

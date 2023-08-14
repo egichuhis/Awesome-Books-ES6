981d31dc15581bf7e8f0bf79e69676a297c05037
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-14 23:56:46 +0300

    Fix Linter errors:css,js

--- index.js
+++ index.js
@@ -5,30 +5,23 @@ import showBooks from './modules/showBooks.js';
 import addBook from './modules/addBook.js';
 
 const myBooks = [];
+const navigation = document.querySelector('nav');
 const addBtn = document.getElementById('add-btn');
-const addBooksLink = document.getElementById('add-books-link');
-const listBooksLink = document.getElementById('list-books-link');
-const contactLink = document.getElementById('contact-link');
 const currentTime = document.getElementById('current-time');
+
+navigation.addEventListener('click', (event) => {
+  const { target } = event;
+  if (target.tagName === 'A') {
+    event.preventDefault();
+    const sectionId = target.getAttribute('href');
+    toggleSectionVisibility(sectionId);
+  }
+});
 
 addBtn.addEventListener('click', () => {
   addBook(myBooks);
 });
 
 currentTime.textContent = getTimeNow();
-
-listBooksLink.addEventListener('click', () => {
-  toggleSectionVisibility('list-books-section');
-});
-
-addBooksLink.addEventListener('click', () => {
-  toggleSectionVisibility('add-books-section');
-});
-
-contactLink.addEventListener('click', () => {
-  toggleSectionVisibility('contact-section');
-});
-
-// Initial display of books
 getStoredBooks(myBooks);
 showBooks(myBooks);

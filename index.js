a47e7ecf15cdd3fbcae81991d8db112f9204e68f
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-14 22:46:01 +0300

    Add inline func for remove btn eventlistener

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import generateUniqueId from './node_modules/generate-unique-id/generateUniqueId.js';
 import getTimeNow from './modules/timeNow.js';
 import toggleSectionVisibility from './modules/toggleSection.js';
 import getStoredBooks from './modules/getStoredBooks.js';
@@ -10,40 +11,13 @@ const listBooksLink = document.getElementById('list-books-link');
 const contactLink = document.getElementById('contact-link');
 const currentTime = document.getElementById('current-time');
 
-const addBook = () => {
-  const bookID = myBooks.length + 1;
-  const bookTitle = document.getElementById('title').value.trim();
-  const bookAuthor = document.getElementById('author').value.trim();
-  const errorSpan = document.getElementById('error-msg');
-  const errorMsg = 'Both Title and Author must be filled out!';
-
-  if (!bookTitle || !bookAuthor) {
-    errorSpan.textContent = errorMsg;
-    return;
-  }
-
-  myBooks.push({ bookID, bookTitle, bookAuthor });
-  localStorage.setItem('myBooks', JSON.stringify(myBooks));
-
-  // Call showBooks() to see all books
-  showBooks();
-
-  // Clear the input fields values
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-};
-
-const removeBook = (bookIDToRemove) => {
-  const myBooksUpdated = myBooks.filter((myBook) => myBook.bookID !== bookIDToRemove);
-  localStorage.setItem('myBooks', JSON.stringify(myBooksUpdated));
-
-  // Update the myBooks array with the updated version
-  myBooks.length = 0;
-  myBooks.push(...myBooksUpdated);
-
-  // Call showBooks() to see all books
-  showBooks();
-};
+// const removeBook = (bookIDToRemove) => {
+//   const myBooksUpdated = myBooks.filter((myBook) => myBook.bookID !== bookIDToRemove);
+//   localStorage.setItem('myBooks', JSON.stringify(myBooksUpdated));
+//   myBooks.length = 0;
+//   myBooks.push(...myBooksUpdated);
+//   window.location.reload();
+// };
 
 const showBooks = () => {
   listBooks.innerHTML = '';
@@ -58,13 +32,44 @@ const showBooks = () => {
     const removeBtn = document.createElement('button');
     removeBtn.classList.add('remove-btn');
     removeBtn.textContent = 'Remove';
+
+    // inline function for the event listener
     removeBtn.addEventListener('click', () => {
-      removeBook(myBook.bookID);
+      const bookIDToRemove = myBook.bookID;
+      const myBooksUpdated = myBooks.filter((myBook) => myBook.bookID !== bookIDToRemove);
+      localStorage.setItem('myBooks', JSON.stringify(myBooksUpdated));
+      myBooks.length = 0;
+      myBooks.push(...myBooksUpdated);
+      // Call the showBooks function to update the displayed books
+      showBooks();
     });
+
     myBookDiv.appendChild(bookDetails);
     myBookDiv.appendChild(removeBtn);
     listBooks.appendChild(myBookDiv);
   });
+};
+
+const addBook = () => {
+  const bookID = generateUniqueId();
+  const bookTitle = document.getElementById('title').value.trim();
+  const bookAuthor = document.getElementById('author').value.trim();
+  const errorSpan = document.getElementById('error-msg');
+  const errorMsg = 'Both Title and Author must be filled out!';
+
+  if (!bookTitle || !bookAuthor) {
+    errorSpan.textContent = errorMsg;
+    return;
+  }
+
+  myBooks.push({ bookID, bookTitle, bookAuthor });
+  localStorage.setItem('myBooks', JSON.stringify(myBooks));
+
+  showBooks();
+
+  // Clear the input fields values
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
 };
 
 addBtn.addEventListener('click', addBook);
